ee0bd94e7918fdfe5ff43125dd51cb9793052c91
EthanOK <sdzdj01@163.com>
2024-03-19 17:34:09 +0800

    modified:   src/gocode/testproject/unit10/demo9/main.go

--- src/gocode/testproject/unit10/demo9/main.go
+++ src/gocode/testproject/unit10/demo9/main.go
@@ -1,5 +1,35 @@
 package main
 
+// 接口的定义
 type SayHello interface {
-	sayHello(s string)
+	sayHello()
+}
+
+// 只要实现了接口的所有方法，就算实现了该接口
+type Chinese struct {
+	name string
+}
+
+func (c Chinese) sayHello() {
+	println("你好", c.name)
+}
+
+type English struct {
+	name string
+}
+
+func (e English) sayHello() {
+	println("Hi", e.name)
+}
+func say(s SayHello) {
+	s.sayHello()
+
+}
+
+func main() {
+	var c SayHello = Chinese{"张三"}
+	var e SayHello = English{"John"}
+	say(c)
+	say(e)
+
 }
